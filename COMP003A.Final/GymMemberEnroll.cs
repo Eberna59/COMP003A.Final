7cc5305859e72b04d993660fa02b280e77be86b6
evanbern59 <evansantos1202@gmail.com>
2026-03-02 21:36:03 -0800

    added first set of string fields

---
+++ COMP003A.Final/GymMemberEnroll.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMP003A.Final
+{
+    internal class GymMemberEnroll
+    {
+        // String fields
+        public string FirstName;
+        public string LastName;
+        public string PhoneNumber;
+        public string Email;
+        public string Address;
+        public string City;
+        public string State;
+        public string PostalCode;
+        public string EmergencyContactName;
+        public string EmergencyContactNumber;
+
+
+
+    }
+}
